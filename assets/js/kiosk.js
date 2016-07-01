234709dfb6c35408df1cdf8cb394c3a4284372dc
Vladimir Smirnov <synteny@user.noreply.github.com>
2016-07-01 16:48:44 +0300

    Initial commit

---
+++ assets/js/kiosk.js
@@ -0,0 +1,76 @@
+function readFile(url, onComplete) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState == 4){
+            onComplete(xmlhttp.responseText)
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+var video_paths = [];
+
+readFile(VIDEO_PATH + VIDEO_LIST_FILENAME, data => {
+    video_paths = data.split("\n");
+});
+
+$(document).ready(function() {
+    var video = $("#video");
+
+    var idleTime = 0;
+    var videoPlaying = false;
+
+    video.css("display", "none");
+
+    video.on('ended', function()
+    {
+        toggleVideo(false);
+    });
+
+    function toggleVideo(visible) {
+        if (visible) {
+            video.fadeIn(FADE_DURATION);
+            video[0].setAttribute('src', 'assets/video/maldives.mp4');
+            video[0].setAttribute('type', 'video/mp4');
+            video[0].currentTime = 0;
+            video[0].load();
+            video[0].play();
+            videoPlaying = true;
+        }
+        else {
+            video.fadeOut(FADE_DURATION);
+            videoPlaying = false;
+        }
+    }
+
+    function timerIncrement() {
+        if (videoPlaying == false) {
+            if (idleTime >= TIMEOUT) {
+                idleTime = 0;
+                toggleVideo(true);
+            }
+            else {
+                idleTime = idleTime + 1;
+            }
+        }
+    }
+
+    function onUserInput() {
+        idleTime = 0;
+        if (videoPlaying) toggleVideo(false);
+    }
+
+    //Increment the idle time counter every second.
+    var idleInterval = setInterval(timerIncrement, 1000);
+
+    $(this).mousemove(function (e) {
+        onUserInput();
+    });
+
+    $(this).keypress(function (e) {
+        onUserInput();
+    });
+});
+
+
